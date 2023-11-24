43cf742ceea0964b0ca15468cd8970727174aef1
rostest <ros.test@lehning.de>
2023-11-24 11:21:35 +0100

    Rebase from 'upstream'

--- test/scripts/run_linux_ros1_bloom.bash
+++ test/scripts/run_linux_ros1_bloom.bash
@@ -11,7 +11,6 @@ generate_prerelease_script.py \
           noetic default ubuntu focal amd64 \
           --custom-repo \
             sick_scan_xd:git:https://github.com/SICKAG/sick_scan_xd:feature/bloom_pretest \
-            msgpack11:git:https://github.com/SICKAG/msgpack11:master \
             libsick_ldmrs:git:https://github.com/SICKAG/libsick_ldmrs:master \
           --level 1 \
           --output-dir ./        
